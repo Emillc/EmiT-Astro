203313193bddcf2fb00032006c47868da7952f0d
chillcicada <2210227279@qq.com>
2024-07-23 01:49:02 +0800

    feat: add unocss as default css engine

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config'
+import unocss from 'unocss/astro'
 import icon from 'astro-icon'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     icon(),
+    unocss(),
   ],
 })
--- eslint.config.js
+++ eslint.config.js
@@ -4,4 +4,5 @@ import antfu from '@antfu/eslint-config'
 export default antfu({
   formatters: true,
   astro: true,
+  unocss: true,
 })
