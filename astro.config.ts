d1c76fa46b9c8139867354460776f291c61995e1
chillcicada <2210227279@qq.com>
2024-07-23 02:03:04 +0800

    update(deps): remove astro-icon, use unocss presetIcon instead

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config'
 import unocss from 'unocss/astro'
-import icon from 'astro-icon'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    icon(),
     unocss(),
   ],
 })
